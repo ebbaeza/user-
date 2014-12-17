11127b11bbd24126d28579553ab1a3470e5bae98
Your Name <you@example.com>
2014-12-17 20:25:28 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,34 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  def show 
+  @bio = Bio.find_by_id(params['id'])
+end
+    render 'show'
+  end
+  def new
+  end
+  def create
+  b = Bio.new
+  b.biography = params['biography']
+  b.name = params['name']
+  b.url_image = params['url_image']
+  b.save
+  redirect_to "/bio/#{ g.id }"
+end
+
+  def update
+  b = Bio.find_by_id(params['id'])
+  b.biography = params['biography']
+    b.name = params['name']
+  b.url_image = params['url_image']
+  b.save
+  redirect_to "/bio/#{ g.id }"
+end
+def destroy
+  b rail server
+  = Bio.find_by_id(params['id'])
+  b.destroy
+end
+  end
